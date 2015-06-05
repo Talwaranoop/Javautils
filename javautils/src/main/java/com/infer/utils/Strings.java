3474d4321231b3dc504defe2a5efa8852733f5de
Anoop <anoop@inferess.com>
2015-06-05 21:19:22 +0530

    added javadoc

--- javautils/src/main/java/com/infer/utils/Strings.java
+++ javautils/src/main/java/com/infer/utils/Strings.java
@@ -3,13 +3,27 @@ package com.infer.utils;
 import java.io.*;
 
 /**
- * Created by Anoop on 5/22/2015.
+ * The Strings program implements an application that
+ * simply converts a string to uppercase or lowercase
+ * by using different methods.
+ *
+ * @author Anoop
+ * @since 5/22/2015.
  */
 public class Strings {
 
-    //convert a string to lower case
+    /**
+     * This method converts a string to Lower Case.
+     *
+     * @param s     This is the first parameter and is used to input a string.
+     * @param start This parameter has been used to declare the starting point
+     *              from which the string will be LowerCased.
+     * @param len   This parameter has been used to declare the length up to
+     *              which the string will be LowerCased.
+     * @return This returns the converted LowerCased string.
+     */
     public static String toLowerCase(String s, int start, int len) {
-        char[] charArray = s.toCharArray();
+        char[] charArray = s.toCharArray();//Stores sting in array
         for (int i = start; i < start + len; i++) {
             if (Character.isUpperCase(charArray[i])) {
                 charArray[i] = Character.toLowerCase(charArray[i]);
@@ -18,7 +32,16 @@ public class Strings {
         return new String(charArray);
     }
 
-    //convert a string to upper case
+    /**
+     * This method converts a string to upper case.
+     *
+     * @param s     This is the first parameter and is used to input a string.
+     * @param start This parameter has been used to declare the starting point
+     *              from which the string will be UpperCased.
+     * @param len   This parameter has been used to declare the length up to
+     *              which the string will be UpperCased.
+     * @return This returns the converted UpperCased string.
+     */
     public static String toUpperCase(String s, int start, int len) {
         char[] charArray = s.toCharArray();
         for (int i = start; i < start + len; i++) {
@@ -29,31 +52,50 @@ public class Strings {
         return new String(charArray);
     }
 
-    //convert a string to lower case using Byte
-    public static void toLowerCase(byte[] byteStream,int start, int len){
+    /**
+     * This method converts a string to lower case using Byte.
+     *
+     * @param byteStream This para
+     * @param start      This parameter has been used to declare the starting point
+     *                   from which the string will be LowerCased.
+     * @param len        This parameter has been used to declare the length up to
+     *                   which the string will be LowerCased.
+     */
+    public static void toLowerCase(byte[] byteStream, int start, int len) {
         try {
-            for(int i = start; i < start + len;i++){
-                if(byteStream[i] >= 65 && byteStream[i] <= 90){
+            for (int i = start; i < start + len; i++) {
+                if (byteStream[i] >= 65 && byteStream[i] <= 90) {
                     byteStream[i] = (byte) (byteStream[i] + 32);
+                    /**
+                     *65 & 90 are ascii codes of byte representing uppercase
+                     *  alphabets A & Z respectively.
+                     *Where as the '32' also known as Delta is the difference between
+                     * an uppercase and a lower case alphabet.
+                     */
                 }
             }
             System.out.println(new String(byteStream));
-        }catch(IndexOutOfBoundsException e){
+        } catch (IndexOutOfBoundsException e) {
             e.printStackTrace();
         }
     }
 
     //convert a string to upper case using Byte
-    public static void toUpperCase(byte[] byteStream,int start, int len){
+    public static void toUpperCase(byte[] byteStream, int start, int len) {
         try {
-            for(int i = start; i < start + len;i++){
-                if(byteStream[i] >= 97 && byteStream[i] <= 122){
+            for (int i = start; i < start + len; i++) {
+                if (byteStream[i] >= 97 && byteStream[i] <= 122) {
                     byteStream[i] = (byte) (byteStream[i] - 32);
+                    /**97 & 122 are ascii codes of byte representing characters lowercase
+                     * letters a & z respectively.
+                     * Where as the '32' also known as Delta is the difference between
+                     * an uppercase and a lower case letter.
+                     */
                 }
             }
             System.out.println(new String(byteStream));
-        }catch(IndexOutOfBoundsException e){
+        } catch (IndexOutOfBoundsException e) {
             e.printStackTrace();
         }
     }
-    }
+}
--- javautils/src/test/java/com/infer/utils/StringsTest.java
+++ javautils/src/test/java/com/infer/utils/StringsTest.java
@@ -1,59 +1,57 @@
 package com.infer.utils;
-import org.junit.Test ;
+
+import org.junit.Test;
 
 import static com.infer.utils.Strings.toLowerCase;
 import static com.infer.utils.Strings.toUpperCase;
-import static org.junit.Assert.* ;
+import static org.junit.Assert.*;
+
 /**
  * Created by Anoop on 6/1/2015.
  */
 public class StringsTest {
 
     @Test
-    public void test_LowerCase(){
-        System.out.println("Test if String is Lower cased.. ");
+    public void testLowerCase() {
         Strings s = new Strings();
         assertTrue(toLowerCase("THIS IS ANOOP", 4, 4).equals("THIS is ANOOP"));
     }
 
     @Test
-    public void test_UpperCase(){
-        System.out.println("Test if String is Upper cased..");
+    public void testUpperCase() {
         Strings s = new Strings();
         assertTrue(toUpperCase("this is anoop", 4, 4).equals("this IS anoop"));
     }
 
     @Test
-    public void test_voidLowercase(){
-        System.out.println("Test if String is Lower cased..");
+    public void testvoidLowercase() {
         Strings s = new Strings();
         assertTrue(toLowerCase("THIS IS ANOOP", 4, 4).equals("THIS is ANOOP"));
     }
 
     @Test
-    public void test_voidUpperCase(){
-        System.out.println("Test if String is Upper cased..");
+    public void testvoidUpperCase() {
         Strings s = new Strings();
         assertTrue(toUpperCase("this is anoop", 4, 4).equals("this IS anoop"));
     }
 
     @Test
-    public void testTest_LowerCase() throws Exception {
+    public void testTestLowerCase() throws Exception {
 
     }
 
     @Test
-    public void testTest_UpperCase() throws Exception {
+    public void testTestUpperCase() throws Exception {
 
     }
 
     @Test
-    public void testTest_voidLowercase() throws Exception {
+    public void testTestvoidLowercase() throws Exception {
 
     }
 
     @Test
-    public void testTest_voidUpperCase() throws Exception {
+    public void testTestvoidUpperCase() throws Exception {
 
     }
 }
