4a9e8721a6af7c8c8d9c9aaccadc352e8c6c4c2a
vishal <vishal@localhost>
2015-06-10 20:54:16 -0700

    Implemented shouldConvertBytesToLowerCaseFromStartToEndIndices test case

--- javautils/src/test/java/com/infer/utils/StringsTest.java
+++ javautils/src/test/java/com/infer/utils/StringsTest.java
@@ -10,26 +10,33 @@ import static com.infer.utils.Strings.*;
  */
 public class StringsTest {
 
-    @Test
+  @Test
 
-    public void testLowerCase() throws IllegalArgumentException {
+  public void testLowerCase() throws IllegalArgumentException {
 
-        Assert.assertEquals(toLowerCase(null, 2, 0), (null));
-    }
+    Assert.assertEquals(toLowerCase(null, 2, 0), (null));
+  }
 
-    @Test
-    public void testUpperCase() {
-        Assert.assertEquals(toUpperCase("anoop", 2, -1), ("anoop"));
-    }
+  @Test
+  public void testUpperCase() {
+    Assert.assertEquals(toUpperCase("anoop", 2, -1), ("anoop"));
+  }
 
-    @Test
-    public void testVoidLowercase() {
-        Assert.assertEquals(toByteLowerCase(, 0, 0),();
-    }
+  @Test
+  public void shouldConvertBytesToLowerCaseFromStartToEndIndices() {
+    String testCase = "THIS IS A TEST";
+    String expected = "This is a test";
+    byte[] bytes = testCase.getBytes();
+    int start = testCase.indexOf("H");
+    int end = testCase.length() - start;
+    // upper case bytes should be converted to lowercase
+    Strings.toByteLowerCase(bytes, start, end);
+    Assert.assertEquals(new String(bytes), expected);
+  }
 
-    @Test
-    public void testVoidUpperCase() {
-        Assert.assertEquals(toByteUpperCase("", 4, 4), ("this IS anoop"));
-    }
+  @Test
+  public void testVoidUpperCase() {
+    //Assert.assertEquals(toByteUpperCase("".getBytes(), 4, 4), ("this IS anoop"));
+  }
 
 }
