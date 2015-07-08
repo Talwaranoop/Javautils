a3b4772d50b1586d9901fe89dfe7383d3f834370
vishal <vishal@localhost>
2015-07-08 11:56:34 -0700

    Multiple sentences in a test case

--- javautils/src/test/java/com/infer/utils/WordFrequencyTest.java
+++ javautils/src/test/java/com/infer/utils/WordFrequencyTest.java
@@ -1,91 +1,105 @@
-    package com.infer.utils;
+package com.infer.utils;
 
-    import org.junit.Before;
-    import org.junit.Rule;
-    import org.junit.Test;
-    import org.junit.rules.ExpectedException;
-    import java.util.HashMap;
-    import java.util.Map;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-    import static org.hamcrest.core.IsEqual.equalTo;
+import java.util.HashMap;
+import java.util.Map;
 
-    /**
-     * The WordFrequencyTest program checks the various conditions
-     * through different test cases to make sure that for different
-     * inputs, the correct expected outputs or actions happen.
-     *
-     * Also,it makes sure that for bad inputs, correct handling
-     * occurs and the expected outputs or actions still happen.
-     *
-     * @author Anoop
-     * @since  6/19/2015.
-     */
-    public class WordFrequencyTest {
+import static org.hamcrest.core.IsEqual.equalTo;
 
-        private WordFrequency obj;
-        @Before
-        public void executedBeforeEach() {
-            obj = new WordFrequency();
-        }
-        @Test
-        /**This test case iterates over the sentences given as
-         * input and counts the frequency of dictionary words.
-         */
-        public void shouldCountDictionaryWordsInSentences(){
-            Map<String, Integer> mp;
-            EntityDictionary dictionary = new EntityDictionary();
-            dictionary.add("Anoop");
-            dictionary.add("Baba");
-            dictionary.add("Cat");
-            WordFrequency wordFrequency = new WordFrequency(dictionary);
-            mp = wordFrequency.getWordFrequency("Anoop can't be Baba and Anoop can never be D and D can never be Cat ");
-            for(String key: mp.keySet())
-            {
-                System.out.println(key + ": " + mp.get(key));
+/**
+ * The WordFrequencyTest program checks the various conditions
+ * through different test cases to make sure that for different
+ * inputs, the correct expected outputs or actions happen.
+ * <p>
+ * Also,it makes sure that for bad inputs, correct handling
+ * occurs and the expected outputs or actions still happen.
+ *
+ * @author Anoop
+ * @since 6/19/2015.
+ */
+public class WordFrequencyTest {
 
-            }
-         }
+  private WordFrequency obj;
 
+  @Before
+  public void executedBeforeEach() {
+    obj = new WordFrequency();
+  }
 
-        @Rule
-        public ExpectedException thrown = ExpectedException.none();
+  @Test
+  /**This test case iterates over the sentences given as
+   * input and counts the frequency of dictionary words.
+   */
+  public void shouldCountDictionaryWordsInSentences() {
+    EntityDictionary dictionary = new EntityDictionary();
+    dictionary.add("Apple");
+    dictionary.add("Samsung");
+    dictionary.add("Tesla");
+    dictionary.add("Exxon");
+    dictionary.add("Vioin");
+    dictionary.add("Google");
+    dictionary.add("Twitter");
+    dictionary.add("Microsoft");
 
-        @Test
-        /**This test case confirms that if the null string in entered
-         * as input the correct handling occurs.
-         */
-        public void throwsIllegalArgumentExceptionWhenNullStringIsGiven() {
-            // arrange
-            thrown.expect(IllegalArgumentException.class);
-            thrown.expectMessage(equalTo("The string cannot be null!"));
-            // act
-            obj.getWordFrequency(null);
-        }
-        @Test
-        /**This test case confirms that if the empty string in entered
-         * as input the correct handling occurs.
-         */
-        public void throwsIllegalArgumentExceptionWhenEmptyStringIsGiven() {
-            // arrange
-            thrown.expect(IllegalArgumentException.class);
-            String input = "";
-            thrown.expectMessage(equalTo("String cannot be empty"));
-            // act
-            obj.getWordFrequency(input);
+    WordFrequency wordFrequency = new WordFrequency(dictionary);
 
+    Map<String, Integer> wordFrequencies = new HashMap<>();
 
-        }
+    Map<String, Integer> wordFrequency1 = wordFrequency.getWordFrequency("Apple iphone has out numbered Samsung galaxy");
+    Map<String, Integer> wordFrequency2 = wordFrequency.getWordFrequency("Tesla and Google are showcasing their self driving cars");
+    Map<String, Integer> wordFrequency3 = wordFrequency.getWordFrequency("Can Microsoft take a lead on Google Android");
+    Map<String, Integer> wordFrequency4 = wordFrequency.getWordFrequency("Google may acquire Twitter");
 
-        @Test
-        /**This test case confirms that if the null Dictionary is entered
-         * as input the correct handling occurs.
-         */
-        public void throwsIllegalArgumentExceptionWhenNullDictionaryIsGiven() {
-            // arrange
-            thrown.expect(IllegalArgumentException.class);
-            thrown.expectMessage(equalTo("The Dictionary cannot be null!"));
-            // act
-            WordFrequency wordFrequency = new WordFrequency(null);
-
-        }
+    for (String key : wordFrequency1.keySet()) {
+      System.out.println(key + ": " + wordFrequencies.get(key));
     }
+  }
+
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  /**This test case confirms that if the null string in entered
+   * as input the correct handling occurs.
+   */
+  public void throwsIllegalArgumentExceptionWhenNullStringIsGiven() {
+    // arrange
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage(equalTo("The string cannot be null!"));
+    // act
+    obj.getWordFrequency(null);
+  }
+
+  @Test
+  /**This test case confirms that if the empty string in entered
+   * as input the correct handling occurs.
+   */
+  public void throwsIllegalArgumentExceptionWhenEmptyStringIsGiven() {
+    // arrange
+    thrown.expect(IllegalArgumentException.class);
+    String input = "";
+    thrown.expectMessage(equalTo("String cannot be empty"));
+    // act
+    obj.getWordFrequency(input);
+
+
+  }
+
+  @Test
+  /**This test case confirms that if the null Dictionary is entered
+   * as input the correct handling occurs.
+   */
+  public void throwsIllegalArgumentExceptionWhenNullDictionaryIsGiven() {
+    // arrange
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage(equalTo("The Dictionary cannot be null!"));
+    // act
+    WordFrequency wordFrequency = new WordFrequency(null);
+
+  }
+}
